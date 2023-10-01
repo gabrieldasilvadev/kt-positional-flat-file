7d278e58273040c242c175f5c5cc7bd226b6e2b0
gabs <gabrieldasilvaprivado1@gmail.com>
2023-10-01 11:27:00 -0300

    create extension function for remove accents

--- src/main/kotlin/extensions/Extensions.kt
+++ src/main/kotlin/extensions/Extensions.kt
@@ -1,5 +1,11 @@
 package extensions
 
+import java.text.Normalizer
+
 fun String.removeDotAndComma(): String {
     return this.replace(".", "").replace(",", "")
+}
+
+fun String.unaccented(): String {
+    return Normalizer.normalize(this, Normalizer.Form.NFD).replace("[^\\p{ASCII}]".toRegex(), "")
 }
