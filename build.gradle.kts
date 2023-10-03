5617b982a92132e703ddd6b0b92d4edaa713cec4
gabs <gabrieldasilvaprivado1@gmail.com>
2023-10-03 01:24:38 -0300

    refactoring generateFile method to static and return ByteArrayInputStream

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import java.net.URI
+import java.util.Base64
 import org.jreleaser.model.Active
 
 plugins {
@@ -10,7 +11,7 @@ plugins {
 }
 
 group = "com.github.gabrieldasilvadev"
-version = "1.0.9"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -20,6 +21,61 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.21")
     testImplementation(kotlin("test"))
 }
+publishing {
+    repositories.maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+        name = "OSSRH"
+
+        credentials {
+            username = System.getenv("OSSRH_USER")
+            password = System.getenv("OSSRH_KEY")
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            pom {
+                name.set("Kotlin Positional Flat File")
+                description.set("Lib for generate positional txt file")
+                url.set("https://github.com/gabrieldasilvadev/kt-positional-flat-file")
+//                licenses {
+//                    license {
+//                        name.set("Apache-2.0")
+//                        distribution.set("repo")
+//                        url.set("")
+//                    }
+//                }
+
+                developers {
+                    developer {
+                        id.set("gabrieldasilvadev")
+                        name.set("Gabriel da Silva")
+                        email.set("gabrieldasilvadev@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:ssh://github.com/gabrieldasilvadev/kt-positional-flat-file.git")
+                    developerConnection.set("scm:git:ssh://github.com/gabrieldasilvadev/kt-positional-flat-file.git")
+                    url.set("https://github.com/gabrieldasilvadev/kt-positional-flat-file")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKeyId: String? = System.getenv("SIGNING_KEY_ID")
+    val signingPassword: String? = System.getenv("SIGNING_PASSWORD")
+    val signingKey: String? = System.getenv("SIGNING_KEY")?.let { base64Key ->
+        String(Base64.getDecoder().decode(base64Key))
+    }
+
+    if (signingKeyId != null) {
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+        sign(publishing.publications)
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
--- src/main/kotlin/PositionalFileProcessor.kt
+++ src/main/kotlin/PositionalFileProcessor.kt
@@ -4,6 +4,7 @@ import exceptions.InvalidFieldSizeException
 import exceptions.MandatoryException
 import extensions.removeDotAndComma
 import extensions.unaccented
+import java.io.ByteArrayInputStream
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.time.LocalDate
@@ -11,118 +12,116 @@ import java.time.format.DateTimeFormatter
 import kotlin.math.min
 
 class PositionalFileGenerator() {
-    fun <T : Any> generateFile(data: List<T>, batchSize: Int? = null): ByteArray {
-        val concatenatedLines = StringBuilder()
-        val batchSizeFinal = batchSize ?: BATCH_SIZE_DEFAULT
-        val totalBatches = (data.size + batchSizeFinal - 1) / batchSizeFinal
+    companion object {
+        private const val BATCH_SIZE_DEFAULT = 10_000
+        fun <T : Any> generateFile(data: List<T>, batchSize: Int? = null): ByteArrayInputStream {
+            val concatenatedLines = StringBuilder()
+            val batchSizeFinal = batchSize ?: BATCH_SIZE_DEFAULT
+            val totalBatches = (data.size + batchSizeFinal - 1) / batchSizeFinal
 
-        for (batchIndex in 0..<totalBatches) {
-            val start = batchIndex * batchSizeFinal
-            val end = min(start + batchSizeFinal, data.size)
+            for (batchIndex in 0..<totalBatches) {
+                val start = batchIndex * batchSizeFinal
+                val end = min(start + batchSizeFinal, data.size)
 
-            for (index in start..<end) {
-                val line = generatePositionalLine(data[index])
-                concatenatedLines.append(line).append('\n')
-            }
-        }
-
-        return stringToByteArray(concatenatedLines.toString())
-    }
-
-    private fun <T : Any> generatePositionalLine(data: T): String {
-        val clazz = data::class
-        val properties = clazz.java.declaredFields.sortedBy { field ->
-            field.getAnnotation(PositionalField::class.java)?.position ?: Int.MAX_VALUE
-        }
-        val stringBuilder = StringBuilder()
-        for (property in properties) {
-            val positionalFieldAnnotation = property.getAnnotation(PositionalField::class.java)
-            if (positionalFieldAnnotation != null) {
-                property.isAccessible = true
-                var fieldValue = property.get(data)?.toString() ?: ""
-                val length = positionalFieldAnnotation.size
-                val paddingChar = positionalFieldAnnotation.paddingChar
-                val mask = positionalFieldAnnotation.mask
-                val fractionalDigits = positionalFieldAnnotation.fractionalDigits
-                val uppercase = positionalFieldAnnotation.upperCase
-                val align = positionalFieldAnnotation.paddingAlign
-                val regex = positionalFieldAnnotation.regex
-                val mandatory = positionalFieldAnnotation.mandatory
-
-                val regexToValidateFieldIsLocalDate = """^\d{4}-\d{2}-\d{2}$""".toRegex()
-                val regexToValidateFieldIsMoney = """^\d{1,3}(\.\d+)${'$'}""".toRegex()
-
-                if (mandatory && fieldValue.isBlank()) {
-                    throw MandatoryException(property.name)
+                for (index in start..<end) {
+                    val line = generatePositionalLine(data[index])
+                    concatenatedLines.append(line).append('\n')
                 }
+            }
 
-                val valueNew: String = buildString {
+            return stringToByteArray(concatenatedLines.toString())
+        }
 
-                    if (regex.isNotEmpty()) {
-                        fieldValue = fieldValue.replace(Regex(regex), "")
+        private fun <T : Any> generatePositionalLine(data: T): String {
+            val clazz = data::class
+            val properties = clazz.java.declaredFields.sortedBy { field ->
+                field.getAnnotation(PositionalField::class.java)?.position ?: Int.MAX_VALUE
+            }
+            val stringBuilder = StringBuilder()
+            for (property in properties) {
+                val positionalFieldAnnotation = property.getAnnotation(PositionalField::class.java)
+                if (positionalFieldAnnotation != null) {
+                    property.isAccessible = true
+                    var fieldValue = property.get(data)?.toString() ?: ""
+                    val length = positionalFieldAnnotation.size
+                    val paddingChar = positionalFieldAnnotation.paddingChar
+                    val mask = positionalFieldAnnotation.mask
+                    val fractionalDigits = positionalFieldAnnotation.fractionalDigits
+                    val uppercase = positionalFieldAnnotation.upperCase
+                    val align = positionalFieldAnnotation.paddingAlign
+                    val regex = positionalFieldAnnotation.regex
+                    val mandatory = positionalFieldAnnotation.mandatory
+
+                    val regexToValidateFieldIsLocalDate = """^\d{4}-\d{2}-\d{2}$""".toRegex()
+                    val regexToValidateFieldIsMoney = """^\d{1,3}(\.\d+)${'$'}""".toRegex()
+
+                    if (mandatory && fieldValue.isBlank()) {
+                        throw MandatoryException(property.name)
                     }
 
-                    if (fieldValue.matches(regexToValidateFieldIsLocalDate)) {
-                        val localDate = LocalDate.parse(fieldValue, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-                        fieldValue = localDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
-                    }
+                    val valueNew: String = buildString {
 
-                    fieldValue = if (!uppercase) {
-                        fieldValue.lowercase()
-                    } else {
-                        fieldValue.uppercase()
-                    }
+                        if (regex.isNotEmpty()) {
+                            fieldValue = fieldValue.replace(Regex(regex), "")
+                        }
 
-                    if (mask.isNotEmpty()) {
-                        val localDate = LocalDate.parse(fieldValue, DateTimeFormatter.ofPattern("yyyyMMdd"))
-                        fieldValue = localDate.format(DateTimeFormatter.ofPattern(mask))
-                    }
+                        if (fieldValue.matches(regexToValidateFieldIsLocalDate)) {
+                            val localDate = LocalDate.parse(fieldValue, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                            fieldValue = localDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+                        }
 
-                    if (fractionalDigits > 0) {
-                        if (fieldValue.isNotBlank()) {
-                            val value = BigDecimal(fieldValue)
-                            val scale = value.scale()
-                            val zerosToAdd = fractionalDigits - scale
+                        fieldValue = if (!uppercase) {
+                            fieldValue.lowercase()
+                        } else {
+                            fieldValue.uppercase()
+                        }
 
-                            fieldValue = if (zerosToAdd > 0) {
-                                val scaledValue = value.setScale(fractionalDigits, RoundingMode.HALF_UP)
-                                scaledValue.toString().replace(".", "").removeDotAndComma()
-                            } else {
-                                fieldValue.removeDotAndComma()
+                        if (mask.isNotEmpty()) {
+                            val localDate = LocalDate.parse(fieldValue, DateTimeFormatter.ofPattern("yyyyMMdd"))
+                            fieldValue = localDate.format(DateTimeFormatter.ofPattern(mask))
+                        }
+
+                        if (fractionalDigits > 0) {
+                            if (fieldValue.isNotBlank()) {
+                                val value = BigDecimal(fieldValue)
+                                val scale = value.scale()
+                                val zerosToAdd = fractionalDigits - scale
+
+                                fieldValue = if (zerosToAdd > 0) {
+                                    val scaledValue = value.setScale(fractionalDigits, RoundingMode.HALF_UP)
+                                    scaledValue.toString().removeDotAndComma()
+                                } else {
+                                    fieldValue.removeDotAndComma()
+                                }
                             }
                         }
+
+                        if (fieldValue.matches(regexToValidateFieldIsMoney)) {
+                            fieldValue = fieldValue.removeDotAndComma()
+                        }
+
+                        append(fieldValue.take(length))
                     }
 
-                    if (fieldValue.matches(regexToValidateFieldIsMoney)) {
-                        fieldValue = fieldValue.removeDotAndComma()
+                    val formattedValue = when (align) {
+                        PaddingAlign.END -> valueNew.padEnd(length, paddingChar)
+                        PaddingAlign.START -> valueNew.padStart(length, paddingChar)
                     }
 
-
-                    append(fieldValue.take(length))
+                    if (length > 0 && formattedValue.length != length) {
+                        throw InvalidFieldSizeException(
+                            property.name,
+                            length,
+                            length
+                        )
+                    }
+                    stringBuilder.append(formattedValue.unaccented())
                 }
-
-                val formattedValue = when (align) {
-                    PaddingAlign.END -> valueNew.padEnd(length, paddingChar)
-                    PaddingAlign.START -> valueNew.padStart(length, paddingChar)
-                }
-
-                if (length > 0 && formattedValue.length != length) {
-                    throw InvalidFieldSizeException(
-                        property.name,
-                        length,
-                        length
-                    )
-                }
-                stringBuilder.append(formattedValue.unaccented())
             }
+
+            return stringBuilder.toString()
         }
 
-        return stringBuilder.toString()
-    }
-
-    private fun stringToByteArray(inputString: String) = inputString.toByteArray(Charsets.UTF_8)
-
-    companion object {
-        const val BATCH_SIZE_DEFAULT = 10_000
+        private fun stringToByteArray(inputString: String) = inputString.toByteArray(Charsets.UTF_8).inputStream()
     }
 }
