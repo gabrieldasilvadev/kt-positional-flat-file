346cd8ef83027c44fe6b5a5b656bb83932f06efc
gabs <gabrieldasilvaprivado1@gmail.com>
2023-10-04 01:11:38 -0300

    refactoring code and handler money values

--- src/main/kotlin/annotations/PositionalField.kt
+++ src/main/kotlin/annotations/PositionalField.kt
@@ -1,17 +1,17 @@
 package annotations
 
-import enums.PaddingAlign
+import enums.PadAlign
 
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class PositionalField(
     val position: Int,
     val size: Int,
+    val padAlign: PadAlign = PadAlign.END,
+    val padChar: Char = ' ',
     val regex: String = "",
-    val paddingAlign: PaddingAlign = PaddingAlign.END,
     val mask: String = "",
-    val fractionalDigits: Int = 0,
+    val fractionDigits: Int = 0,
     val upperCase: Boolean = true,
-    val paddingChar: Char = ' ',
     val mandatory: Boolean = false
 )
